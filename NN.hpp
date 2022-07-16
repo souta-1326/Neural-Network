c1d092dadb8162ce7791d75aa38db0042dcdbe96
souta-1326 <62239382+souta-1326@users.noreply.github.com>
2022-07-16 13:03:46 +0900

    Update NN.hpp
    Act_Aに活性化されたAの値を入れることで、それを計算する回数を1回にしました。
    Aの大きさはWの大きさよりずっと小さいので、メモリ面ではあまり変わらないと思います。

--- NN.hpp
+++ NN.hpp
@@ -18,12 +18,12 @@ template <F(*Hidden_Act)(F),F(*Hidden_Act_dash)(F),F(*Output_Act)(F),F(*Output_A
   static constexpr int layer_count = sizeof...(_node_count);
   static constexpr int node_count[layer_count] = {_node_count...};
   static constexpr int max_node_count = *max_element(node_count,node_count+layer_count);
-  F A[layer_count][max_node_count+1];
+  F A[layer_count][max_node_count+1],Act_A[layer_count][max_node_count+1];
   F W[layer_count-1][max_node_count+1][max_node_count],dW[layer_count-1][max_node_count+1][max_node_count];
   F lr;
 public:
   NN(F _lr = 0.01):lr(_lr){
-    for(int i=0;i<layer_count-1;i++) A[i][node_count[i]] = 1;
+    for(int i=0;i<layer_count-1;i++) A[i][node_count[i]] = Act_A[i][node_count[i]] = 1;
     random_device seed_gen;
     default_random_engine engine(seed_gen());
     for(int i=0;i<layer_count-1;i++){
@@ -45,19 +45,20 @@ public:
   void training(F x[node_count[0]],F t[node_count[layer_count-1]]){
     //順伝播
     memcpy(A[0],x,node_count[0]*sizeof(F));
+    memcpy(Act_A[0],A[0],node_count[0]*sizeof(F));
     for(int i=0;i<layer_count-1;i++){
       fill(A[i+1],A[i+1]+node_count[i+1],0);
       for(int k=0;k<node_count[i]+1;k++){
-        F bef_A = (i==0 || k==node_count[i] ? A[i][k]:Hidden_Act(A[i][k]));
-        for(int j=0;j<node_count[i+1];j++) A[i+1][j] += bef_A*W[i][k][j];
+        for(int j=0;j<node_count[i+1];j++) A[i+1][j] += Act_A[i][k]*W[i][k][j];
       }
+      for(int j=0;j<node_count[i+1];j++) Act_A[i+1][j] = (i==layer_count-2 ? Output_Act:Hidden_Act)(A[i+1][j]);
     }
     //逆伝播
     static F delta[max_node_count],delta_new[max_node_count];//転置されている
     for(int i=layer_count-1;i>=1;i--){
       if(i == layer_count-1){
         for(int j=0;j<node_count[i];j++){
-          delta[j] = Output_Act_dash(A[i][j])*(Output_Act(A[i][j])-t[j]);
+          delta[j] = Output_Act_dash(A[i][j])*(Act_A[i][j]-t[j]);
         }
       }
       else{
@@ -69,7 +70,7 @@ public:
         memcpy(delta,delta_new,node_count[i]*sizeof(F));
       }
       for(int j=0;j<node_count[i-1]+1;j++){
-        F nex_A = (i==1 || j==node_count[i-1] ? A[i-1][j]:Hidden_Act(A[i-1][j]));
+        F nex_A = Act_A[i-1][j];
         for(int k=0;k<node_count[i];k++) dW[i-1][j][k] = nex_A*delta[k];
       }
     }

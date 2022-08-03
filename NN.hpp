543545df744e77ae5d603291c4acda5dd6c99da6
souta-1326 <souta.tahara@gmail.com>
2022-08-03 12:41:01 +0900

    update

--- NN.hpp
+++ NN.hpp
@@ -1,35 +1,34 @@
 #include<bits/stdc++.h>
-using namespace std;
 using F = float;
 inline void sigmoid(int N,F A[],F ret[]){
-  for(int i=0;i<N;i++) ret[i] = 1/(1+exp(-A[i]));
+  for(int i=0;i<N;i++) ret[i] = 1/(1+std::exp(-A[i]));
 }
 inline F sigmoid_dash(F x){
-  F s = 1/(1+exp(-x));
+  F s = 1/(1+std::exp(-x));
   return s*(1-s);
 }
 inline F sigmoid_dash(F x,F y,F t){
-  F s = 1/(1+exp(-x));
+  F s = 1/(1+std::exp(-x));
   return s*(1-s)*(y-t);
 }
 inline void id(int N,F A[],F ret[]){memcpy(ret,A,N*sizeof(F));}
 inline F id_dash(F x,F y,F t){return (y-t);}
 inline void tanh(int N,F A[],F ret[]){
-  for(int i=0;i<N;i++) ret[i] = tanh(A[i]);
+  for(int i=0;i<N;i++) ret[i] = std::tanh(A[i]);
 }
 inline F tanh_dash(F x){
-  F cosh_x = cosh(x);return 1/(cosh_x*cosh_x);
+  F cosh_x = std::cosh(x);return 1/(cosh_x*cosh_x);
 }
 inline void softmax(int N,F A[],F ret[]){
   F sum = 0;
-  for(int i=0;i<N;i++) sum += (ret[i] = exp(A[i]));
+  for(int i=0;i<N;i++) sum += (ret[i] = std::exp(A[i]));
   for(int i=0;i<N;i++) ret[i] /= sum;
 }
 inline F softmax_dash(F x,F y,F t){
   return y-t;
 }
 inline void relu(int N,F A[],F ret[]){
-  for(int i=0;i<N;i++) ret[i] = max(A[i],F(0));
+  for(int i=0;i<N;i++) ret[i] = std::max(A[i],F(0));
 }
 inline F relu_dash(F x){
   return x>=0;
@@ -37,12 +36,12 @@ inline F relu_dash(F x){
 template <void(*Hidden_Act)(int,F[],F[]),F(*Hidden_Act_dash)(F),void(*Output_Act)(int,F[],F[]),F(*Output_loss_Act_dash)(F,F,F),int... _node_count> class NN{
   static constexpr int layer_count = sizeof...(_node_count);
   static constexpr int node_count[layer_count] = {_node_count...};
-  static constexpr int max_node_count = *max_element(node_count,node_count+layer_count);
+  static constexpr int max_node_count = *std::max_element(node_count,node_count+layer_count);
   //F A[layer_count][max_node_count+1],Act_A[layer_count][max_node_count+1];
   F **A,**Act_A;
   //F W[layer_count-1][max_node_count_left+1][max_node_count_right],dv[layer_count-1][max_node_count_left+1][max_node_count_right];
   F ***W,***dv,***ds;//重み,motentum,RMSProp
-  F alpha;
+  F alpha;//学習率
   F momentum_beta;
   F sub_1_momentum_beta;
   F momentum_beta_powt;
@@ -86,10 +85,10 @@ public:
       }
     }
     for(int i=0;i<layer_count-1;i++) A[i][node_count[i]] = Act_A[i][node_count[i]] = 1;
-    random_device seed_gen;
-    default_random_engine engine(seed_gen());
+    std::random_device seed_gen;
+    std::default_random_engine engine(seed_gen());
     for(int i=0;i<layer_count-1;i++){
-      normal_distribution dist(0.0,2/sqrt(node_count[i]));
+      std::normal_distribution dist(0.0,1/sqrt(node_count[i]));
       for(int j=0;j<node_count[i]+1;j++){
         for(int k=0;k<node_count[i+1];k++){
           W[i][j][k] = dist(engine);
@@ -108,7 +107,7 @@ public:
     //順伝播
     memcpy(Act_A[0],x,node_count[0]*sizeof(F));
     for(int i=0;i<layer_count-1;i++){
-      fill(A[i+1],A[i+1]+node_count[i+1],0);
+      std::fill(A[i+1],A[i+1]+node_count[i+1],0);
       for(int k=0;k<node_count[i]+1;k++){
         for(int j=0;j<node_count[i+1];j++) A[i+1][j] += Act_A[i][k]*W[i][k][j];
       }
@@ -123,7 +122,7 @@ public:
         }
       }
       else{
-        fill(delta_new,delta_new+node_count[i],0);
+        std::fill(delta_new,delta_new+node_count[i],0);
         for(int j=0;j<node_count[i];j++){
           for(int k=0;k<node_count[i+1];k++) delta_new[j] += W[i][j][k]*delta[k];
           delta_new[j] *= Hidden_Act_dash(A[i][j]);
@@ -166,7 +165,7 @@ public:
   void output(F x[node_count[0]],F ret[node_count[layer_count-1]]){
     memcpy(Act_A[0],x,node_count[0]*sizeof(F));
     for(int i=0;i<layer_count-1;i++){
-      fill(A[i+1],A[i+1]+node_count[i+1],0);
+      std::fill(A[i+1],A[i+1]+node_count[i+1],0);
       for(int k=0;k<node_count[i]+1;k++){
         for(int j=0;j<node_count[i+1];j++) A[i+1][j] += Act_A[i][k]*W[i][k][j];
       }
